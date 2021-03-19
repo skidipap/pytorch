db7c05f938ef3d412515aaec10090fcf2cbcb0eb
Kimish Patel <kimishpatel@fb.com>
2021-03-19 15:25:20 -0700

    [PyTorch, Mobile] Serialization format change for source range
    Summary:
    In order to bring mobile deployment, via lite interpreter, on feature
    parity with JIT, with respect model level debug information we must make
    model level debug information available to mobile runtime.
    At the moment, model level debug information is stored in SourceRange
    which associates node's of graph to where the come from in original
    python source code.
    This information is serialized as part of debug_pkl and deserialized
    when JIT loads the model and reads the model code.
    On lite interpreter, we do not have access to all the functionality of
    JIT and hence we cannot load model in the same way as JIT, by reading
    code, constructing module hierarchy and graph corresponding module
    methods etc. Instead in, lite interpreter, only bytecode corresonding to
    the compiled graph, Code, is saved.
    Thus in order to annotate OPs in the bytecode with equivalent
    SourceRange information we do the following:
    1. During model serialization, we create a unique tag for each source
    range of the model.
    2. Create a map of <SourceRange, tag>
    3. During debug_pkl serialization we save tag along with SourceRange, on
    top of byte offset.
    4. During bytecode generation, the methods of the top module are
    lowered. During this process methods are inlined. In the inlined graph,
    when the node of a graph is lowered to bytecode, we query node's source
    range and look it up against the map.
    5. Resulting source range tag is serialized in module_debug_info.
    6. During model deserialization, we read all the debug_pkl records in
    the archieve and create a map of <tag, SourceRange>
    7. This map can be used to find source code information.
    
    During mobile runtime:
    1. We read all the debug_pkl records and create <tag=debug_handle,
    SourceRange> map.
       1.1 This map, MobileDebugInfo, is a member of mobile Module.
    2. Interpreter catches appropriate exceptions and sets the thread local
    debug handle and rethrows the exception.
    3. In Function's run method we catch exception and query current debug
    handle where the exception happened.
    4. Query MobileDebugInfo with debug handle to retrieve source range and
    augment error with source range info.
    
    This information is still incomplete as it does not contain entire
    callstack.
    
    In the following diffs we will serialize InlinedCallStack directly.
    
    Note that compilation is gated by SYMBOLICATE_MOBILE_DEBUG_HANDLE macro,
    so that mobile builds can avoid building MobileDebugInfo, source range
    and source range pickler/unpickler. Later we will add path where, if
    building without debug support stack trace will contain only debug
    handles. They can be symbolicated later.
    
    Test Plan:
    Ported bunch of source range tests from test_jit.py. Added on more test
    in test_lite_interpreter.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: b0fefae4ea81a76129ee456adebd1ed8ad5cd961
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54284

---
+++ torch/csrc/jit/mobile/debug_info.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <c10/util/flat_hash_map.h>
+#include <caffe2/serialize/inline_container.h>
+#include <torch/csrc/jit/frontend/source_range.h>
+
+namespace torch {
+namespace jit {
+class MobileDebugTable {
+ public:
+  MobileDebugTable(
+      std::unique_ptr<caffe2::serialize::PyTorchStreamReader>& reader);
+  std::string getSourceDebugString(const int64_t debug_handle);
+
+ private:
+  ska::flat_hash_map<int64_t, SourceRange> source_range_map_;
+};
+
+} // namespace jit
+} // namespace torch
